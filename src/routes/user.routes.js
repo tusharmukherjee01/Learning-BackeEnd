bc3a2ba55591495cb55115c75ebff63a6d95a233
tusharmukherjee01 <tm18473@gmail.com>
2024-12-11 10:03:03 +0530

    add route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,16 @@
 
 import {Router} from 'express'
-import { registerUser,loginUser,logoutUser,refreshAccessToken } from '../controlers/user.controller.js' // ai rokom import tpkhn e nite parbo like {} jokhn defalt export hoyni mane same name e use korte hobee  
+import { registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails,
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile,
+    getWatchHistory } from '../controlers/user.controller.js' // ai rokom import tpkhn e nite parbo like {} jokhn defalt export hoyni mane same name e use korte hobee  
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from '../middlewares/auth.middleware.js'
 const router = Router()
@@ -23,11 +33,24 @@ router.route("/login").post(loginUser)
 
 //secured route
 router.route("/logout").post(verifyJWT,logoutUser)
+
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar) // this is the steps for file updat using multer  
+
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/histort").get(verifyJWT,getWatchHistory)
+
 export default router
-
-
-
 
 
 /*
